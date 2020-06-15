8f15aa6a3674b76cc8b4521359811bf1e9edb363
Reham-Omar <rehamalsobh96@gmail.com>
2020-06-15 11:42:49 +0300

    l

--- driver.js
+++ driver.js
@@ -8,11 +8,11 @@ function pickUp(payload){
   setTimeout(()=> {
     console.log(`DRIVER: picked up ${payload.orderId}`);
     events.emit('in-transit', payload);
-    setTimeout(()=>{
-      events.emit('delivered', payload);
-    }, 3000);
-
+    
   }, 1000);
+  setTimeout(()=>{
+    events.emit('delivered', payload);
+  }, 3000);
 }
 
 module.exports = pickUp;
